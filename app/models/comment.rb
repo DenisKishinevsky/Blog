52ffca6daff56a2f34bf2708350b66960fb37031
Denis Kishinevsky <info@dlstudio.ca>
2011-05-23 16:22:17 -0400

    test

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+  belongs_to :post
+  
+  validates :commenter,  :presence => true
+  validates :body, :presence => true 
+            
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c8217d88e31870103f9156a3bf92bf885d28de9ec79b1d70fa7d0a7deb5b180a2dfd25f8b38661503d414b3e89c6c1c7d40c29659fcd1716e1b496ad5d1a73d8'
